86f584f31a5bd17d7cfb805f2cc7a095dd1f724a
Ludovic Saffroy <ludo@neuviemepage.com>
2018-07-27 11:08:04 +0200

    PHP search script

--- api/search.php
+++ api/search.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
     define( 'WP_USE_THEMES', false );
     require_once '../wp-load.php';
 
-    $search_term = isset($_GET['s']) ? $_GET['s'] : 'paulin';
+    $search_term = isset($_GET['s']) ? $_GET['s'] : 'lacoste';
 
     $query = new WP_Query( array( 's' => $search_term ) );
 
@@ -17,13 +17,16 @@ header('Content-Type: application/json');
         $yearofwork = get_field('yearofwork', $post->ID);
         $typeofwork = get_field('typeofwork', $post->ID);
         $secondTitle = get_field('second_title', $post->ID);
+        $image = get_field('news_img_small', $post->ID);
 
         $formatPost = array(
             'title' => $post->post_title,
             'year' => $yearofwork,
             'second_title' => $secondTitle,
-            'client' => $client
-        );
+            'client' => $client,
+            'link' => get_bloginfo('url') . '/' . $post->post_name,
+            'image' => get_the_post_thumbnail_url()
+        ); 
 
         array_push($formatPosts,$formatPost);
     }
